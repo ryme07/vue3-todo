ba09f4915323baa751ef2011c25ca0ece4b6c89c
ryme07 <remydelrieu@hotmail.fr>
2022-06-28 20:52:44 +0200

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    About view
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+// const props = defineProps({
+//   msg: { type: String, required: true },
+// });
+
+defineProps({
+  msg: { type: String, required: true },
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
